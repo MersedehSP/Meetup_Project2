5f17bb247bf308bed78be53c67b73d749b53dd39
Mersedeh Saniepay <mersedeh.saniepay@gmail.com>
2021-01-20 20:33:16 -0500

    ERD stuff

---
+++ QuickDBD-export (2).sql
@@ -0,0 +1,37 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Events" (
+    "event_id" int   NOT NULL,
+    "event_name" VARCHAR   NOT NULL,
+    "group_name" VARCHAR   NOT NULL,
+    "event_link" VARCHAR   NOT NULL,
+    "attendees_num" Int   NOT NULL,
+    "city_id" int   NOT NULL,
+    "map_link" VARCHAR   NOT NULL,
+    "event_city" VARCHAR   NOT NULL,
+    "event_street" VARCHAR   NOT NULL,
+    "event_lat" float   NOT NULL,
+    "event_lng" float   NOT NULL,
+    "event_state" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Events" PRIMARY KEY (
+        "event_id"
+     )
+);
+
+CREATE TABLE "City" (
+    "city_id" int   NOT NULL,
+    "state" VARCHAR   NOT NULL,
+    "city" VARCHAR   NOT NULL,
+    "lat" float   NOT NULL,
+    "lng" float   NOT NULL,
+    "population" Int   NOT NULL,
+    CONSTRAINT "pk_City" PRIMARY KEY (
+        "city_id"
+     )
+);
+
+ALTER TABLE "Events" ADD CONSTRAINT "fk_Events_city_id" FOREIGN KEY("city_id")
+REFERENCES "City" ("city_id");
+
