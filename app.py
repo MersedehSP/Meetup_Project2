ed236805e9e6a9ce7715bf7c8c6aad172a1a4827
Joseph Lubrano <jlubranos@optimum.net>
2021-01-24 13:49:27 -0500

    01-24-2021

--- app.py
+++ app.py
@@ -19,19 +19,14 @@ migrate = Migrate(app, db)
 
 engine=create_engine("postgresql://postgres:postgres@127.0.0.1:5432/Meetup") 
 
-#general Flask Code
-
 @app.route('/')
 def home():
 
     return render_template('index.html')
 
-@app.route('/eventLocations')
-def eventLocations():
+@app.route('/heatMap')
+def heatMap():
     session = Session(bind=engine)
-#    results=session.query(meetupEvents.event_lat, meetupEvents.event_lng, meetupEvents.event_city, func.count(meetupEvents.event_name).label('total')).\
-#                        group_by(meetupEvents.event_lat,meetupEvents.event_lng,meetupEvents.event_city).\
-#                        order_by(meetupEvents.event_city).all()
     results=session.query(meetupEvents.event_lat, meetupEvents.event_lng, meetupEvents.event_city).\
                         order_by(meetupEvents.event_city).all()
     result_dict=[]
@@ -42,9 +37,153 @@ def eventLocations():
         record['lng']=lng
         result_dict.append(record)
     session.close()
-#  group_by(meetupEvents.event_lat,meetupEvents.event_lng,meetupEvents.event_city).\
     return jsonify(result_dict)
- 
+
+@app.route('/markerMap')
+@app.route('/markerMap/<city>')
+def markerMap(city='All'):
+
+    session = Session(bind=engine)
+    if (city=='All'):
+        results=session.query(meetupEvents.event_name, meetupEvents.event_lat, meetupEvents.event_lng, meetupEvents.venue_event_link, meetupEvents.attendees, meetupEvents.google_map_link).all()
+        result_dict=[]
+        for name, lat, lng, link, attendees, gmap in results:
+            record={}
+            record['name']=name
+            record['lat']=lat
+            record['lng']=lng
+            record['link']=link
+            record['attendees']=attendees
+            record['gmap']=gmap
+            result_dict.append(record)
+        session.close()
+    else:
+        results=session.query(meetupEvents.event_name, meetupEvents.event_lat,meetupEvents.event_lng, 
+            meetupEvents.venue_event_link, meetupEvents.attendees, meetupEvents.google_map_link).\
+            filter(meetupEvents.event_city==city).all()
+
+        result_dict=[]
+        for name, lat, lng, link, attendees, gmap in results:
+            record={}
+            record['name']=name
+            record['lat']=lat
+            record['lng']=lng
+            record['link']=link
+            record['attendees']=attendees
+            record['gmap']=gmap
+            result_dict.append(record)
+        session.close()        
+    return jsonify(result_dict)
+
+@app.route('/citydropDown')
+@app.route('/citydropDown/<state>')
+def citydropDown(state='All'):
+
+    session = Session(bind=engine)
+    if (state=='All'):
+        results=session.query(meetupEvents.event_city).\
+            order_by(meetupEvents.event_city).all()
+        result_dict=[]
+        for city in results:
+            record={}
+            record['city']=city
+            result_dict.append(record)
+        session.close()
+    else:
+        results=session.query(meetupEvents.event_city).\
+            filter(meetupEvents.event_state==state).\
+            group_by(meetupEvents.event_city).\
+            order_by(meetupEvents.event_city).all()
+
+        result_dict=[]
+        for city in results:
+            record={}
+            record['city']=city
+            result_dict.append(record)
+        session.close()        
+    return jsonify(result_dict)    
+
+@app.route('/categorydropDown')
+def categorydropDown():
+
+    session = Session(bind=engine)
+    results=session.query(meetupEvents.event_category).\
+        order_by(meetupEvents.event_category).all()
+    result_dict=[]
+    for category in results:
+        record={}
+        record['category']=category
+        result_dict.append(record)
+    session.close()
+    return jsonify(result_dict)
+
+@app.route('/dataTable')
+@app.route('/dataTable/<state>/<city>/<category>')
+def dataTable(state='All', city='All',category='All'):
+
+    session = Session(bind=engine)
+    if (state=='All' and city=='All' and category=='All'):
+        results=session.query(meetupEvents.event_name, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state!='All' and city=='All' and category=='All'):
+        results=session.query(meetupEvents.event_name, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_state==state).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state!='All' and city!='All' and category=='All'):
+        results=session.query(meetupEvents.event_name, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_state==state and meetupEvents.event_city==city).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state!='All' and city!='All' and category!='All'):   
+        results=session.query(meetupEvents.event_city, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_state==state and meetupEvents.event_city==city and meetupEvents.event_category==category).\
+                group_by(meetupEvents.event_city).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state=='All' and city!='All' and category=='All'):   
+        results=session.query(meetupEvents.event_city, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_city==city).\
+                group_by(meetupEvents.event_city).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state=='All' and city!='All' and category!='All'):   
+        results=session.query(meetupEvents.event_city, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_city==city and meetupEvents.event_category==category).\
+                group_by(meetupEvents.event_city).\
+                order_by(meetupEvents.event_city).all() 
+
+    elif (state=='All' and city=='All' and category!='All'):   
+        results=session.query(meetupEvents.event_city, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_category==category).\
+                group_by(meetupEvents.event_city).\
+                order_by(meetupEvents.event_city).all()
+
+    elif (state!='All' and city=='All' and category!='All'):   
+        results=session.query(meetupEvents.event_city, meetupEvents.group_name,
+                meetupEvents.google_map_link, meetupEvents.attendees).\
+                filter(meetupEvents.event_state==state and meetupEvents.event_category==category).\
+                group_by(meetupEvents.event_city).\
+                order_by(meetupEvents.event_city).all()  
+
+    result_dict=[]
+    for city, group, gmap, attendees in results:
+        record={}
+        record['city']=city
+        record['group']=group
+        record['attendees']=attendees
+        record['gmap']=gmap
+        result_dict.append(record)
+    session.close()        
+    return jsonify(result_dict)           
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,3 +1,31 @@
-d3.json('/eventLocations').then((response)=> {
+d3.json('/heatMap').then((response)=> {
     console.log(response)
 })
+
+d3.json('/markerMap').then((response)=> {
+    console.log(response)
+})
+
+d3.json('/markerMap/Albany').then((response)=> {
+    console.log(response)
+})
+
+d3.json('/citydropDown').then((response)=> {
+    console.log(response)
+})
+
+d3.json('/citydropDown/NY').then((response)=> {
+    console.log(response)
+})
+
+// d3.json('/categorydropDown').then((response)=> {
+//    console.log(response)
+//})
+
+// d3.json('/dataTable').then((response)=> {
+//    console.log(response)
+//})
+
+// d3.json('/dataTable/<state>/<city>/<category>').then((response)=> {
+//    console.log(response)
+//})
