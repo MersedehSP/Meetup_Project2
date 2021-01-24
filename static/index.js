1ecaf0e02b2cb092ded7392ea735a9ce32fb3770
Joseph Lubrano <jlubranos@optimum.net>
2021-01-24 14:08:23 -0500

    01-24-2021

--- static/index.js
+++ static/index.js
@@ -1,21 +1,21 @@
 d3.json('/heatMap').then((response)=> {
-    console.log(response)
+    console.log("heatmap event loactions : ",response)
 })
 
 d3.json('/markerMap').then((response)=> {
-    console.log(response)
+    console.log("marker map all locations : ",response)
 })
 
 d3.json('/markerMap/Albany').then((response)=> {
-    console.log(response)
+    console.log("marker map with city selection : ",response)
 })
 
 d3.json('/citydropDown').then((response)=> {
-    console.log(response)
+    console.log("city drop down all states : ",response)
 })
 
 d3.json('/citydropDown/NY').then((response)=> {
-    console.log(response)
+    console.log("city drop down selected state : ",response)
 })
 
 // d3.json('/categorydropDown').then((response)=> {
