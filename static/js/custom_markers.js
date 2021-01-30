7ebecad1a1ac8f9a69db9bca9a5dc39119c59c21
Veena Uppalapati <vuveenauppalapati@gmail.com>
2021-01-29 21:02:16 -0500

    Adding markers js files

---
+++ static/js/custom_markers.js
@@ -0,0 +1,52 @@
+var coffeeMarker = L.AwesomeMarkers.icon({
+    icon: 'coffee', 
+    prefix: 'fa',
+    markerColor: 'yellow'
+  });
+
+
+var photographyMarker = L.AwesomeMarkers.icon({
+    icon: 'camera', 
+    prefix: 'fa',
+    markerColor: 'green'
+  });
+
+
+var musicMarker =  L.AwesomeMarkers.icon({
+    icon: 'headphones', 
+    prefix: 'fa',
+    markerColor: 'orange'
+  });
+
+var filmMarker = L.AwesomeMarkers.icon({
+    icon: 'film', 
+    prefix: 'fa',
+    markerColor: 'blue'
+  });
+
+var familyMarker = L.AwesomeMarkers.icon({
+    icon: 'group', 
+    prefix: 'fa',
+    markerColor: 'blue'
+  });
+
+var socialMarker = L.AwesomeMarkers.icon({
+    icon: 'group', 
+    prefix: 'fa',
+    markerColor: 'green'
+});
+
+var LGBTQMarker = L.AwesomeMarkers.icon({
+    icon: 'group', 
+    prefix: 'fa',
+    markerColor: 'purple'
+});
+
+var techMarker = L.AwesomeMarkers.icon({
+    icon: 'desktop', 
+    prefix: 'fa',
+    markerColor: 'yellow'
+  });
+
+
+
