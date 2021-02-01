092001c7d40750eb654fe9359e894dd99aeaad16
Veena Uppalapati <vuveenauppalapati@gmail.com>
2021-01-31 20:19:03 -0500

    Removed unnnecessary directories

--- Ramyata/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiZ2V0YXBpa2V5IiwiYSI6ImNranEybGNhNDB5N3cyc2szaW05ODB6bmMifQ.siLluftLb4OInylLP2f0pg";
--- event_finder/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-var API_KEY = "pk.eyJ1IjoidXBwYWx2ZSIsImEiOiJja2p1a3BqcHcybjRoMnVseWoyenJjOHhhIn0.XTBpJ8DE_jjBuj-C7OKoZA";
